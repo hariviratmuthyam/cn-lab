b46df9fca536099f52388bc1ebc43233be350979
labexams <86876820+labexams@users.noreply.github.com>
2021-11-29 20:49:32 +0530

    Create bit_stuffing.c

---
+++ bit_stuffing.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+    int in[50],out[50],n,i=0,j=0,count,x;
+    printf("Enter length of data:");
+    scanf("%d", &n);
+    printf("Enter the data:");
+    for (x = 0; x < n; x++)
+        scanf("%d", &in[x]);
+    while (i < n) {
+        count=0;
+        if (in[i] == 1)
+        {
+            out[j] = in[i];
+            while(i<n && in[i]==1)
+            {
+                j++,i++,count++;
+                out[j] = in[i];
+                if(count == 5)
+                {
+                    j++;
+                    out[j] = 0;
+                }
+            }
+        }
+        else
+            out[j] = in[i];
+        i++,j++;
+    }
+    printf("Result:");
+    for (i = 0; i < j; i++)
+        printf("%d", out[i]);
+    return 0;
+}
